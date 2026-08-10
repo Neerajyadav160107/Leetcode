1e96fe65a24628dfe61bdddb75688b1bb0a46863
NEERAJ YADAV <neerajyadav160107@gmail.com>
2026-08-10 21:49:35 +0530

    Time: 0 ms (100%), Space: 14.9 MB (56.07%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -3,12 +3,12 @@ public:
     int diagonalSum(vector<vector<int>>& mat) {
         int m = mat.size();
         int sum = 0;
-        for (int i = 0; i <=m-1; i++) {
-            sum += mat[i][i];
-            if (i != m - 1 - i) {
-            sum += mat[i][m - 1 - i];
-            }       
+        for (int i=0;i<mat.size();i++){
+            sum+= mat[i][i];
+            if ( i!=m-1-i){
+                sum+= mat[i][m-1-i];
+            }
         }
         return sum;
-    }    
+    }
 };
