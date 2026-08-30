9d683bb213e13ff33f207ed58f542782710128e9
NEERAJ YADAV <neerajyadav160107@gmail.com>
2026-08-30 09:36:20 +0530

    Time: 0 ms (100%), Space: 21 MB (34.33%) - LeetHub

---
+++ 4038-count-integers-appearing-in-a-single-block/4038-count-integers-appearing-in-a-single-block.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+        unordered_set<int> seen;
+        unordered_set<int> bad;
+        seen.insert(nums[0]);
+        for (int i =1; i<nums.size(); i++) {
+            // New block starts
+            if (nums[i]!=nums[i - 1]) {
+                if (seen.count(nums[i])) {
+                    bad.insert(nums[i]);
+                }
+                else {
+                    seen.insert(nums[i]);
+                }
+            }
+        }
+        int ans = 0;
+
+        for (int x : seen) {
+            if (!bad.count(x)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
