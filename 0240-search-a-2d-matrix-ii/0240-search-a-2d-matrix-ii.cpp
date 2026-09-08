11ba3c35e0d5502eb24298e1c47f6f804e4c430d
NEERAJ YADAV <neerajyadav160107@gmail.com>
2026-09-08 20:49:12 +0530

    Time: 43 ms (88%), Space: 18.5 MB (92.88%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int i = 0;
+        int j = matrix[0].size() - 1;
+
+        while (i < matrix.size() && j >= 0) {
+            int current = matrix[i][j];
+
+            if (current > target)
+                j--;
+                else if (current < target)
+                    i++;
+                    else return true;
+        }
+        return false;
+    }
+};
